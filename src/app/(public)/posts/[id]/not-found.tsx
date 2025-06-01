50249c1a44f1470c3822b7d3ec143b2d3b3968dc
bookkuun <bookkuun@gmail.com>
2025-06-01 18:00:53 +0900

    記事詳細

---
+++ src/app/(public)/posts/[id]/not-found.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>お探しの記事は存在しないか、削除された可能性があります。</div>;
+};
+
+export default NotFound;
--- src/lib/post.ts
+++ src/lib/post.ts
@@ -7,3 +7,10 @@ export async function getPosts() {
     orderBy: { createdAt: "desc" },
   });
 }
+
+export async function getPost(id: string) {
+  return await prisma.post.findUnique({
+    where: { id },
+    include: { author: { select: { name: true } } },
+  });
+}
