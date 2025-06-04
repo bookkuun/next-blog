2004f4dcfc6f184639a61f2c72f10bc8b768c0e8
bookkuun <bookkuun@gmail.com>
2025-06-04 10:53:58 +0900

    記事一覧機能

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -8,7 +8,7 @@ export default function PrivateLayout({
   return (
     <>
       <PrivateHeader />
-      {children}
+      <div className="container mx-auto p-4 py-8">{children}</div>
     </>
   );
 }
