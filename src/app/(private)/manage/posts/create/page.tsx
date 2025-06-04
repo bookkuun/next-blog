9d9f071848d959575b6e04c6b38ff234d289c1b1
bookkuun <bookkuun@gmail.com>
2025-06-04 23:02:42 +0900

    不要なimport

--- src/app/(private)/manage/posts/create/page.tsx
+++ src/app/(private)/manage/posts/create/page.tsx
@@ -10,7 +10,6 @@ import "highlight.js/styles/github.css";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Divide } from "lucide-react";
 
 const CreatePage = () => {
   const [content, setContent] = useState("");
